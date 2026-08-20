7b623a81c25dadac3428b11ebb2f1ba5ab960b7b
Talesxx <56258069+Talesxx@users.noreply.github.com>
2026-08-20 03:15:58 +0000

    Deploying to gh-pages from @ Talesxx/myDiary@c40580c8b5f62e54a36b114031cf51407a9ace52 🚀

--- assets/chunks/architecture-TIHT7OUA.DkCWBuEE.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-KEIR6QF5.BICK3FdT.js";import{M as e}from"./mermaid-parser.core.DcCWJ8Zx.js";export{e as createArchitectureServices};
---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.Do1reiZS.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./src.QN9XHvqB.js";import"./chunk-DU6HZSFF.B6ihz6iq.js";import"./chunk-75Z2AOVW.PuMrUUvW.js";import"./chunk-P2QGCYS3.DOsaeCjz.js";import"./chunk-PWAF6VOD.BbXT460O.js";import"./chunk-GMAD6QVW.DYYhkXN6.js";import"./chunk-TEH6E4GO.CWzZyfsY.js";import"./chunk-OBVCFTLP.Cht-0SQT.js";import"../app.CwaP1hWG.js";import"./chunk-F27PBJKO.BOCHWnhc.js";import"./chunk-XXDRQBXY.BTiQUmv6.js";import"./chunk-POPQ4Y6H.BPfpbQEe.js";import{r as t,t as n}from"./chunk-RHFEMEQ7.DJCJA12L.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
