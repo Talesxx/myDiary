279b9eb860388c27eecaf2471788240993b8185e
Talesxx <56258069+Talesxx@users.noreply.github.com>
2026-09-13 09:52:14 +0000

    Deploying to gh-pages from @ Talesxx/myDiary@e992cc08fe9521c2f459b50cb0497734282a8850 🚀

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.D57AxBOS.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./src.QN9XHvqB.js";import"./chunk-DU6HZSFF.R5q99Xsp.js";import"./chunk-75Z2AOVW.C2wNIDWH.js";import"./chunk-P2QGCYS3.4WhAJmD8.js";import"./chunk-PWAF6VOD.DbhdZU1H.js";import"./chunk-GMAD6QVW.DhparAWa.js";import"./chunk-4HAMMTFA.CxaEC4pb.js";import"./chunk-GVQU2GXP.BEq6yG7T.js";import"../app.CW2CnsMU.js";import"./chunk-F27PBJKO.Bdn8NJCK.js";import"./chunk-XXDRQBXY.BTiQUmv6.js";import"./chunk-POPQ4Y6H.ClwdNaIN.js";import{r as t,t as n}from"./chunk-SHT3W25Y.LWJ0S0Fu.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
